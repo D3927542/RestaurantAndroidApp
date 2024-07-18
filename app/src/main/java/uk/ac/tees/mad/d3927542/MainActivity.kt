ccea0910714964b24625f926e3b18cdcafcf021a
Sandhya <D3927542@live.tees.ac.uk>
2024-07-18 22:03:52 +0100

    commit four

--- app/src/main/java/uk/ac/tees/mad/d3927542/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/mad/d3927542/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            //four commit
             RestaurantMenuAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
